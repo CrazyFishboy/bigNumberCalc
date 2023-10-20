e37a0065c6df1ba3f53ad2408b7d1c866d87317f
JMGillum <gillum0927@gmail.com>
2023-10-19 20:02:31 -0700

    Added capacity and negative variables, and changed the convert constructor from unsigned int to convert to int

--- big.h
+++ big.h
@@ -22,15 +22,19 @@ class Big {
 
 public:
 
+    // Constructors and Destructors
     Big() : Big("0") {}
-    Big(unsigned long long val) : Big(std::to_string(val)) {}
+    Big(long long val) : Big(std::to_string(val)) {}
     Big(std::string val);
     Big(const Big& right);
     ~Big();
 
+    // Getters
     std::string getValue() const;
     int getSize() const { return size; }
+    int getCapacity() const { return capacity; }
     int getDigit(int index) const;
+    bool isNegative() const { return negative;}
 
     friend std::ostream& operator<<(std::ostream& out, const Big& object);
     int& operator[ ](int index);
@@ -61,8 +65,10 @@ protected:
 
 
 private:
-    int * digits;
-    int size;
+    int * digits; // Array of digits
+    int size; // Number of digits used
+    int capacity; // Number of digits storable in array
+    bool negative; // If the number is negative
 
 
 

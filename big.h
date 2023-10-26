b28e15969dfddc207b7ef1a1b2a3d5123312747e
JMGillum <gillum0927@gmail.com>
2023-10-25 23:30:15 -0700

    Decrement operator function prototypes

--- big.h
+++ big.h
@@ -49,6 +49,8 @@ public:
     friend Big operator*(const Big& left, const Big& right);
     Big& operator++();
     Big operator++(int);
+    Big& operator--();
+    Big operator--(int);
 
     // Comparison operators
     bool operator==(const Big& object) const;

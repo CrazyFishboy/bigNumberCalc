12e472a2331a08a23a3d40dc1c99a212224ec391
JMGillum <gillum0927@gmail.com>
2023-11-09 10:01:27 -0800

    Added functions and variables for changing the base to display the number in

--- big.h
+++ big.h
@@ -24,6 +24,7 @@ public:
     
 
     static int capacityIncrement;
+    const enum {bin = 2, oct = 8, dec = 10, hex = 16};
 
     // Constructors and Destructors
     Big() : Big("0") {}
@@ -70,11 +71,17 @@ public:
 
     void displayArray(); // Temporary for debugging
 
+    bool setBase(int base);
+    int getBase() const { return base; }
+    void showBase();
+    void hideBase();
+
     // Exceptions
     class DivideByZero {};
     class EmptyArray {};
     class OutOfBounds {};
     class InvalidCharacter {};
+    
 
 protected:
     void addDigit(int num = 1); // Increases the size of the array by one at the beginning
@@ -87,6 +94,7 @@ private:
     int size; // Number of digits used
     int capacity; // Number of digits storable in array
     bool negative; // If the number is negative
+    int base; // The base to display the number in
 
 
 

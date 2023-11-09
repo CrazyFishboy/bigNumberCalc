02f1e1799d953e85e864ac8737150de76047e554
JMGillum <gillum0927@gmail.com>
2023-11-09 10:50:05 -0800

    Finished binary convert and started on hexadecimal convert

--- big.cpp
+++ big.cpp
@@ -1105,7 +1105,17 @@ std::string Big::octal() const{
 
 
 std::string Big::hexadecimal() const{
-    return binary();
+    const std::string convert[2][16] = {{"0000","0001","0010","0011","0100","0101","0110","0111","1000","1001","1010","1011","1100","1101","1110","1111"},
+                                       {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"}};
+    std::string value = binary();
+    int offset = 0;
+    if(value[0] == '-'){
+        offset = 1;
+    }
+
+    return value;
+
+
 }
 
 

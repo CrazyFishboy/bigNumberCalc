e2a27adfec6d506575be81ff474a2defa86d68be
JMGillum <gillum0927@gmail.com>
2023-11-09 10:49:48 -0800

    Removed static member values

--- big.h
+++ big.h
@@ -22,10 +22,6 @@
 class Big {
 
 public:
-    
-    const static std::string hexConvert[16];
-    const static std::string octConvert[16];
-    const static char characters[16];
 
     static int capacityIncrement;
     enum {bin = 2, oct = 8, dec = 10, hex = 16};
